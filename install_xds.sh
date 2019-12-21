61a6920935d141aa08156182bcceb0f42f5969da
Egor Marin <marin@phystech.edu>
2019-12-21 19:51:24 +0300

    Correct XDS installation

--- install_xds.sh
+++ install_xds.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-read -r  -n 1 -p "Install gcloud SDK: " 'mainmenuinput'
+read -r  -n 1 -p "Download and install XDS:" 'mainmenuinput'
 if [[ "$mainmenuinput" = "y" ]]; then
 	mkdir xds
 	cd xds || exit
 	sudo cd /usr/local/bin
 	sudo wget -O- ftp://ftp.mpimf-heidelberg.mpg.de/pub/kabsch/XDS-INTEL64_Linux_x86_64.tar.gz | tar xzvf -
 	sudo ln -sf XDS-INTEL64_Linux_x86_64/* .
+	echo "export PATH=\"\$PATH:${PWD}/XDS-INTEL64_Linux_x86_64\"" >> ~/.bashrc
 else
 	:
 fi

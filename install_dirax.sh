d7dcbf8706ffe186e5d8f50e5eb6966cf4860023
Egor Marin <marin@phystech.edu>
2019-12-21 19:13:15 +0300

    Change uncompressing order

--- install_dirax.sh
+++ install_dirax.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-read -r  -n 1 -p "Install packages: " 'mainmenuinput'
+read -r  -n 1 -p "Download and install Dirax: " 'mainmenuinput'
 if [[ "$mainmenuinput" = "y" ]]; then
 	wget http://www.crystal.chem.uu.nl/distr/dirax/download/dirax1.17-Linux-x86_64.tar.Z
 	mkdir dirax
-	mv dirax1.7-Linux.x86_64.tar.Z dirax 
+	mv dirax1.7-Linux.x86_64.tar.Z dirax/
+	cd dirax || return
 	uncompress dirax1.7-Linux.x86_64.tar.Z
 	tar -xvf dirax1.7-Linux.x86_64.tar
 else

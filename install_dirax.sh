684580469ecba58a2019f0b90c8357e0c1b26b61
Egor Marin <marin@phystech.edu>
2019-12-21 19:26:12 +0300

    Fix installation and test it

--- install_dirax.sh
+++ install_dirax.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-read -r  -n 1 -p "Download and install Dirax: " 'mainmenuinput'
-if [[ "$mainmenuinput" = "y" ]]; then
-	wget http://www.crystal.chem.uu.nl/distr/dirax/download/dirax1.17-Linux-x86_64.tar.Z
-	mkdir dirax
-	mv dirax1.7-Linux.x86_64.tar.Z dirax/
-	cd dirax || return
-	uncompress dirax1.7-Linux.x86_64.tar.Z
-	tar -xvf dirax1.7-Linux.x86_64.tar
-else
-	:
-fi
-
 read -r  -n 1 -p "Install libgfortran " 'mainmenuinput'
 if [[ "$mainmenuinput" = "y" ]]; then
 	sudo apt-get install libgfortran3
@@ -19,14 +7,19 @@ else
 	:
 fi
 
-read -r  -n 1 -p "Add dirax indexer to your /usr/local/src: " 'mainmenuinput'
+read -r  -n 1 -p "Download and install Dirax: " 'mainmenuinput'
 if [[ "$mainmenuinput" = "y" ]]; then
-echo """
+	curl -L "http://www.crystal.chem.uu.nl/distr/dirax/download/dirax1.17-Linux-x86_64.tar.Z" -o dirax.tar.Z
+	mkdir dirax
+	mv dirax.tar.Z dirax/
+	cd dirax || return
+	uncompress dirax.tar.Z
+	tar -xvf dirax.tar
+	echo """
 #!/bin/sh
- exec ${PWD}/dirax \"\$\@\"
+exec ${PWD}/dirax \"\$@\"
 """ | sudo tee -a /usr/local/bin/dirax
 sudo chmod ugo+x /usr/local/bin/dirax
 else
 	:
-fi
-
+fi 

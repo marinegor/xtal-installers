3226e68a4aa717de1ef5138393c1129f5799fd42
Egor Marin <marin@phystech.edu>
2019-12-22 17:43:07 +0300

    Separate download and install

--- install_ccp4.sh
+++ install_ccp4.sh
@@ -6,17 +6,24 @@ if ! [[ "${CCP4_YADISK}" ]]; then
 	exit 0
 fi
 
-if [[ $(which yadisk-direct) ]]; then
+if ! [[ $(which yadisk-direct) ]]; then
 	read -r  -n 1 -p "Install yadisk-direct via pip: " 'mainmenuinput' 
 	if [[ "$mainmenuinput" == "y" ]]; then
-		pip install --user yadisk-direct; fi
+		pip3 install --user yadisk-direct; fi
 fi
+
+read -r  -n 1 -p "Download CCP4:" 'mainmenuinput' 
+if [[ "$mainmenuinput" == "y" ]]; then
+	curl -L "$(yadisk-direct ${CCP4_YADISK})" -o "ccp4-7.0.078-shelx-arpwarp-linux64.tar"
+else
+	:
+fi
+
 
 read -r  -n 1 -p "Install CCP4 without ARP/wARP: " 'mainmenuinput' 
 if [[ "$mainmenuinput" == "y" ]]; then
-	curl -L "$(yadisk-direct ${CCP4_YADISK})" -o "ccp4-7.0.078-shelx-arpwarp-linux64.tar"
-	tar -xvf ccp4-7.0.078-shelx-arpwarp-linux64.tar
-	cd ccp4-7.0 || return
+	tar -xvf "ccp4-7.0.078-shelx-arpwarp-linux64.tar"
+	cd "ccp4-7.0" || return
 	./BINARY.setup
 	echo "## Added by install_ccp4.sh" >> ~/.bashrc
 	echo "source $(pwd)/bin/ccp4.setup-sh" >> ~/.bashrc
